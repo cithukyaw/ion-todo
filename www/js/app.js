98b303f0fbe0feeb3a20bbcf1f1e70986557c7fc
Sithu <cithukyaw@gmail.com>
2015-12-03 22:57:19 +0630

    Creating tasks

--- www/js/app.js
+++ www/js/app.js
@@ -5,13 +5,35 @@
 // the 2nd parameter is an array of 'requires'
 angular.module('todo', ['ionic'])
 
-.controller('TodoCtrl', function($scope) {
-  $scope.tasks = [
-    { title: 'Collect coins' },
-    { title: 'Eat mushrooms' },
-    { title: 'Get hight enought to get the flag' },
-    { title: 'Find the Princess' }
-  ];
+.controller('TodoCtrl', function($scope, $ionicModal) {
+  $scope.tasks = [];
+
+  // Create and load the Modal
+  $ionicModal.fromTemplateUrl('new-task.html', function(modal) {
+    $scope.taskModal = modal;
+  }, {
+    scope: $scope,
+    animation: 'slide-in-up'
+  });
+
+  // Call when the form is submitted
+  $scope.createTask = function(task) {
+    $scope.tasks.push({
+      title: task.title
+    });
+    $scope.taskModal.hide();
+    task.title = '';
+  };
+
+  // Open our new task modal
+  $scope.newTask = function() {
+    $scope.taskModal.show();
+  }
+
+  // Close the new task modal
+  $scope.closeNewTask = function() {
+    $scope.taskModal.hide();
+  }
 })
 
 .run(function($ionicPlatform) {
@@ -25,4 +47,4 @@ angular.module('todo', ['ionic'])
       StatusBar.styleDefault();
     }
   });
-})
+});
